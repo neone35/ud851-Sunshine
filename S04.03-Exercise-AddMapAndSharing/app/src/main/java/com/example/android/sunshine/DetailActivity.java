32354bc9e47c73ac5e7738da9364ef084c407f9d
Artur Maslov <aartasas@gmail.com>
2018-01-01 23:57:02 +0200

    l5-ex3-add_map_sharing

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,12 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,6 +34,36 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem shareItem = menu.add(Menu.NONE, 1, Menu.NONE, "Share");
+        shareItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                shareText(mWeatherDisplay.getText().toString());
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    private void shareText(String sharedText) {
+        String mimeType = "text/plain";
+        String windowTitle = "Share into:";
+
+        ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setChooserTitle(windowTitle)
+                .setText(sharedText)
+                .startChooser();
+    }
+
+    // COMPLETE (3) Create a menu with an item with id of action_share
+    // COMPLETE (4) Display the menu and implement the forecast sharing functionality
 }
