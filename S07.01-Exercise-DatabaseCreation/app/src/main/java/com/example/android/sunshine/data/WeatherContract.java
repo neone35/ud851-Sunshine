f90d355fe43e670974980b8ef9c3cf46c58bad91
Artur Maslov <aartasas@gmail.com>
2018-01-18 00:23:57 +0200

    l8-ex3-unique_constraint

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -21,7 +21,7 @@ import android.provider.BaseColumns;
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
  */
-public class WeatherContract {
+class WeatherContract {
 
     private WeatherContract() {
     }
